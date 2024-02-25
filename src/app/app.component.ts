127cbe39dd7f4708e4568fc96959b9c11abf67c5
sanskar001 <sanskarm001@gmail.com>
2024-02-25 21:13:39 +0530

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PostComponent } from './post/post.component';
-
-/* Chapter: String Interpolation => Expression
-
-  title: string = "Angular Title"
-  <h1>{{title}}</h1>
-  <p>{{10 + 20}}</p> => 30
-
-  Note: We can show string, number, boolean or any other types;
-  Note: It acts like JSX expression in React.
-*/
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, PostComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title: string = 'angular-tutorial';
+  imgUrl: string = 'https://angular.io/assets/images/logos/angular/angular.png';
 
   constructor() {}
 }
